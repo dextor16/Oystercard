92020283070b721d6c5b69b173b6115f356cb39c
dextor16 <jamestcork92@gmail.com>
2019-12-16 15:57:24 +0000

    up to challenge 7

---
+++ lib/oystercard.rb
@@ -0,0 +1,23 @@
+class Oystercard
+
+    MAXIMUM_BALANCE = 90
+
+    attr_reader :balance
+
+    def initialize
+        @balance = 0
+    end
+
+    def balance
+        @balance
+    end
+
+    def top_up(value)
+        raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + value > MAXIMUM_BALANCE
+        @balance = @balance + value
+    end
+
+    def deduct(value)
+        @balance -= value
+    end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,30 @@
-
+require 'oystercard'
 
 describe Oystercard do
+    it 'should show default balance of 0' do
+        expect(subject.balance).to eq (0)
+    end
 
+    it 'should top up balance' do
+        card = Oystercard.new
+        card.top_up(30)
+        expect(card.balance).to eq (30)
+    end
+
+    it 'should deduct balance' do
+        card = Oystercard.new
+        card.top_up(20)
+        card.deduct(10)
+        expect(card.balance).to eq (10)
+    end
+
+    it 'should show error if balance is more than £90' do
+        maximum_balance = (Oystercard::MAXIMUM_BALANCE)
+        subject.top_up(maximum_balance)
+        expect{ subject.top_up (1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+
+
+
+    
 end
